869cfafe8435391f681f9c42de39d67de0b9666b
Emiliano <grimaldi385@gmail.com>
2023-05-09 18:55:01 -0300

    Primer Commit

---
+++ Desafío #02/stark.py
@@ -0,0 +1,5 @@
+from funciones_stark import *
+
+heroes = []
+copiar_datos(lista_personajes,heroes)
+stark_marvel_app(heroes)
---
+++ stark.py
@@ -0,0 +1,6 @@
+from data_stark import *
+from funciones_stark import *
+
+heroes = []
+copiar_datos(lista_personajes,heroes)
+stark_marvel_app(heroes)
